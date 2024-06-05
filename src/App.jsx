a71f28db49cdfbd08123b0865edf50092002a011
achmadsholeh <achmaddsholehh@gmail.com>
2024-06-05 08:41:41 +0700

    React Crud Laravel 10

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+//import Link from react router dom
+import { Link } from "react-router-dom";
+
+//import routes
+import Routes from './routes';
+
+export default function App() {
+
+  return (
+    <>
+      <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-nav">
+          <div class="nav-item text-nowrap">
+
+          </div>
+        </div>
+      </header>
+      <div class="container-fluid">
+        <div class="row">
+          <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <div class="position-sticky pt-3">
+              <ul class="nav flex-column">
+                <li class="nav-item">
+                  <Link to="/" class="nav-link active" aria-current="page">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home" aria-hidden="true"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                    Dashboard
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/posts" className="nav-link active" class="nav-link">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file" aria-hidden="true"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+                    Post
+                  </Link>
+                </li>
+
+              </ul>
+
+
+            </div>
+          </nav>
+
+          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
+            <Routes />
+
+
+          </main>
+        </div>
+      </div>
+
+
+
+    </>
+  )
+
+}
---
+++ src/views/home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="mb-1 bg-light rounded-3">
+            <div className="container-fluid py-1">
+                <h1 className="display-5 fw-bold"> DASHBOARD</h1>
+            </div>
+        </div>
+    )
+}
